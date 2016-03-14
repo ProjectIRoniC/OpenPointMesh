52bac210ad7370b5293bb1cff38155d0139e9972
MattLamont <matthewlamont454@gmail.com>
2016-03-14 00:28:18 -0600

    fixed testing CMakeLists.txt

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -4,6 +4,10 @@
 
 TEST(SquareRootTest, PositiveNos) {
     ASSERT_EQ(6, std::sqrt(36.0));
+
+    vba::CloudStitcher* mCloudStitcher = new vba::CloudStitcher;
+    ASSERT_EQ( 1 , mCloudStitcher->getNumberofFilesRead() );
+    delete mCloudStitcher;
 }
 
 int main(int argc, char **argv) {
