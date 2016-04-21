7a08eb547e8bb23ef26bec0edd1502f65be99dc8
Chris Padgett <chris.e.padgett@gmail.com>
2016-04-21 01:09:22 -0600

    OpenNI2 building

---
+++ SuperBuild/External_OpenNI2_buildonifile.cmake
@@ -0,0 +1,32 @@
+
+# Set directories
+SET( ONIFILE_SOURCE_DIR ${SOURCE_DIR}/Source/Drivers/OniFile )
+
+# Set compile and link flags
+SET( ONIFILE_CXX_FLAGS "${EP_NONCMAKE_COMMON_CXX_FLAGS} -I${XNLIB_INCLUDE_DIR} -I${OPENNI2_LIB_INCLUDE_DIR} -I${ONIFILE_FORMATS_DIR} -I${INTERNAL_JPEG_INCLUDE_DIR}" )
+SET( ONIFILE_LD_FLAGS "${CMAKE_EXE_LINKER_CXX_FLAGS} -L${XNLIB_BIN_DIR} -L${OPENNI2_LIB_BIN_DIR}" )
+
+# Add flags for Position Independent Code
+IF( CMAKE_POSITION_INDEPENDENT_CODE )
+	SET( ONIFILE_CXX_FLAGS "${ONIFILE_CXX_FLAGS} -fPIC" )
+ENDIF()
+
+# Create the build command arguments list
+SET( ONIFILE_BUILD_ARGS CFLAGS=${ONIFILE_CXX_FLAGS} LDFLAGS=${ONIFILE_LD_FLAGS} ALLOW_WARNINGS=1 )
+
+# If needed set debug information
+IF( CMAKE_BUILD_TYPE MATCHES "Debug" )
+	LIST( APPEND ONIFILE_BUILD_ARGS CFG=Debug )
+ENDIF()
+
+# Run the build command
+EXECUTE_PROCESS( COMMAND make ${ONIFILE_BUILD_ARGS}
+			WORKING_DIRECTORY ${ONIFILE_SOURCE_DIR} RESULT_VARIABLE build_result
+)
+
+IF( NOT "${build_result}" STREQUAL "0" )
+	MESSAGE( STATUS "build OniFile Failed!!!" )
+	MESSAGE( FATAL_ERROR "build_result='${build_result}'" )
+ENDIF()
+
+RETURN( ${build_result} )
---
+++ SuperBuild/External_OpenNI2_installopenni2.cmake
@@ -0,0 +1,50 @@
+
+# Install the XnLib library
+MESSAGE( STATUS "installing XnLib library..." )
+EXECUTE_PROCESS( COMMAND ${CMAKE_COMMAND}
+				-E copy_directory ${XNLIB_BIN_DIR} ${INSTALL_DIR}/lib
+				RESULT_VARIABLE install_result
+)
+
+IF( NOT "${install_result}" STREQUAL "0" )
+	MESSAGE( STATUS "install XnLib Failed!!!" )
+	MESSAGE( FATAL_ERROR "install_result='${install_result}'" )
+ENDIF()
+
+# Install the XnLib includes
+MESSAGE( STATUS "installing XnLib includes..." )
+EXECUTE_PROCESS( COMMAND ${CMAKE_COMMAND}
+				-E copy_directory ${XNLIB_INCLUDE_DIR} ${INSTALL_DIR}/include
+				RESULT_VARIABLE install_result
+)
+
+IF( NOT "${install_result}" STREQUAL "0" )
+	MESSAGE( STATUS "install XnLib Failed!!!" )
+	MESSAGE( FATAL_ERROR "install_result='${install_result}'" )
+ENDIF()
+
+# Copy the OpenNI2 bin directory
+MESSAGE( STATUS "installing OpenNI2 bin..." )
+EXECUTE_PROCESS( COMMAND ${CMAKE_COMMAND}
+				-E copy_directory ${OPENNI2_LIB_BIN_DIR} ${INSTALL_DIR}/lib
+				RESULT_VARIABLE install_result
+)
+
+IF( NOT "${install_result}" STREQUAL "0" )
+	MESSAGE( STATUS "install OpenNI2 bin Failed!!!" )
+	MESSAGE( FATAL_ERROR "install_result='${install_result}'" )
+ENDIF()
+
+# Install the OpenNI2 includes
+MESSAGE( STATUS "installing OpenNI2 includes..." )
+EXECUTE_PROCESS( COMMAND ${CMAKE_COMMAND}
+				-E copy_directory ${OPENNI2_LIB_INCLUDE_DIR} ${INSTALL_DIR}/include
+				RESULT_VARIABLE install_result
+)
+
+IF( NOT "${install_result}" STREQUAL "0" )
+	MESSAGE( STATUS "install OpenNI2 includes Failed!!!" )
+	MESSAGE( FATAL_ERROR "install_result='${install_result}'" )
+ENDIF()
+
+RETURN( ${install_result} )
