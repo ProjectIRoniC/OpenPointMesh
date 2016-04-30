a31ee026b4c67c7f0f3f8609f115705f91bef738
Chris Padgett <chris.e.padgett@gmail.com>
2016-04-30 01:20:47 -0600

    Update FontConfig to 2.11.1, added cmake module path to default cache settings

--- SuperBuild/External_FontConfig.cmake
+++ SuperBuild/External_FontConfig.cmake
@@ -20,10 +20,11 @@ SET( ${proj}_DEPENDENCIES
 ExternalProject_Include_Dependencies( ${proj} PROJECT_VAR proj DEPENDS_VAR ${proj}_DEPENDENCIES )
 
 # Set directories
-SET( ${proj}_BUILD_DIR ${CMAKE_CURRENT_BINARY_DIR}/${proj}-build )
+SET( ${proj}_BUILD_DIR ${CMAKE_CURRENT_BINARY_DIR}/${proj} )
 SET( ${proj}_INSTALL_DIR ${CMAKE_CURRENT_BINARY_DIR}/${proj}-install )
 SET( ${proj}_LIBRARY_INSTALL_DIR ${${proj}_INSTALL_DIR}/lib )
 SET( ${proj}_SOURCE_DIR ${SOURCE_DOWNLOAD_CACHE}/${proj} )
+SET( ${proj}_CACHEDIR ${${proj}_SOURCE_DIR}/fc_cache )
 
 ### --- Project specific additions here
 SET( FONTCONFIG_CMAKE_PREFIX_PATH
@@ -36,6 +37,7 @@ SET( FONTCONFIG_CMAKE_PREFIX_PATH
 
 SET( ${proj}_CMAKE_OPTIONS
 	# CMake Build ARGS
+	-DCMAKE_MODULE_PATH:PATH=${CMAKE_MODULE_PATH}
 	-DCMAKE_C_STANDARD:STRING=${CMAKE_C_STANDARD}
 	-DCMAKE_C_STANDARD_REQUIRED:BOOL=${CMAKE_C_STANDARD_REQUIRED}
 	-DCMAKE_INCLUDE_DIRECTORIES_BEFORE:BOOL=${CMAKE_INCLUDE_DIRECTORIES_BEFORE}
@@ -55,22 +57,25 @@ SET( ${proj}_CMAKE_OPTIONS
 )
 
 # Download tar source when possible to speed up build time
-# We are using an older version of FontConfig only because we want to use the CMakeLists.txt file from this repository
-# If updating to a new version of FontConfig that does not have CMakeLists.txt, see commits before 4.27.2016 for alternate External_FontConfig.cmake
-SET( ${proj}_URL https://github.com/LuaDist/fontconfig/archive/2.8.0.tar.gz )
-SET( ${proj}_MD5 30e82444f48ed130b89e5d64ce3819fe )
-# SET( ${proj}_REPOSITORY "${git_protocol}://github.com/LuaDist/fontconfig.git" )
-# SET( ${proj}_GIT_TAG "2.8.0" )
+# We are using a repository where CMakeLists.txt is being maintained for fontconifg
+#SET( ${proj}_URL https://github.com/Mixaill/cmake.fontconfig/archive/2.11.1.tar.gz )
+#SET( ${proj}_MD5 6424af76094b271cfad54d76c9919649 )
+SET( ${proj}_REPOSITORY "${git_protocol}://github.com/Mixaill/cmake.fontconfig.git" )
+SET( ${proj}_GIT_TAG "cmake" )
+
+# We are patching the source CMakeLists.txt to include fcrange.c
+SET( ${proj}_PATCH_COMMAND patch -p1 < ${CMAKE_CURRENT_LIST_DIR}/External_FontConfig_patchsrccmakelists.diff )
 ### --- End Project specfic additions
 
 ExternalProject_Add( ${proj}
 	${${proj}_EP_ARGS}
-	URL					${${proj}_URL}
-	URL_MD5				${${proj}_MD5}
-	# GIT_REPOSITORY	${${proj}_REPOSITORY}
-	# GIT_TAG 			${${proj}_GIT_TAG}
+	# URL				${${proj}_URL}
+	# URL_MD5			${${proj}_MD5}
+	GIT_REPOSITORY		${${proj}_REPOSITORY}
+	GIT_TAG 			${${proj}_GIT_TAG}
+	UPDATE_COMMAND		""	# we are skipping update so we don't have to build every time
 	SOURCE_DIR			${${proj}_SOURCE_DIR}
-	BINARY_DIR			${${proj}_BUILD_DIR}
+	BUILD_IN_SOURCE		1
 	INSTALL_DIR			${${proj}_INSTALL_DIR}
 	LOG_DOWNLOAD		${EP_LOG_DOWNLOAD}
 	LOG_UPDATE			${EP_LOG_UPDATE}
@@ -78,11 +83,64 @@ ExternalProject_Add( ${proj}
 	LOG_BUILD			${EP_LOG_BUILD}
 	LOG_TEST			${EP_LOG_TEST}
 	LOG_INSTALL			${EP_LOG_INSTALL}
+	PATCH_COMMAND		${${proj}_PATCH_COMMAND}
 	CMAKE_GENERATOR		${gen}
 	CMAKE_ARGS			${EP_CMAKE_ARGS}
 	CMAKE_CACHE_ARGS	${${proj}_CMAKE_OPTIONS}
 	DEPENDS				${${proj}_DEPENDENCIES}
 )
+
+# Files: "fcalias.h fcaliastail.h fcftalias.h fcftaliastail.h" generated on Ubuntu by ./configure & make
+IF( CMAKE_SYSTEM_NAME MATCHES "Linux" )
+	# We need to run autogen.sh because it creates a needed file fcstdint.h
+	SET( ${proj}_PREBUILD_CONFIGURE_SCRIPT ${CMAKE_CURRENT_LIST_DIR}/External_FontConfig_preconfigurefontconfig.cmake )
+	ExternalProject_Add_Step( ${proj} run_autogen_sh
+		COMMAND ${CMAKE_COMMAND}
+			# CMake ARGS
+			-DEP_NONCMAKE_COMMON_C_FLAGS:STRING=${EP_NONCMAKE_COMMON_C_FLAGS}
+			-DCMAKE_EXE_LINKER_C_FLAGS:STRING=${CMAKE_EXE_LINKER_C_FLAGS}
+			-DSOURCE_DIR:PATH=${${proj}_SOURCE_DIR}
+			-DINSTALL_DIR:PATH=${${proj}_INSTALL_DIR}
+			-DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
+			-DBUILD_SHARED_LIBS:BOOL=${BUILD_SHARED_LIBS}
+			-DCMAKE_POSITION_INDEPENDENT_CODE:BOOL=${CMAKE_POSITION_INDEPENDENT_CODE}
+			# EXPAT ARGS
+			-DEXPAT_DIR:PATH=${EXPAT_DIR}
+			-DEXPAT_INCLUDE_DIR:PATH=${EXPAT_INCLUDE_DIR}
+			-DEXPAT_LIBRARY_DIR:PATH=${EXPAT_LIBRARY_DIR}
+			# FreeType ARGS
+			-DFREETYPE_INCLUDE_DIR:PATH=${FREETYPE_INCLUDE_DIR}
+			-DFREETYPE_LIBRARY_DIR:PATH=${FREETYPE_LIBRARY_DIR}
+			-DFREETYPE_LIBRARY_NAME:STRING=${FREETYPE_LIBRARY_NAME}
+			# bizp2 ARGS
+			-DBZIP2_LIBRARY_DIR:PATH=${BZIP2_LIBRARY_DIR}
+			-DBZIP2_LIBRARY_NAME:STRING=${BZIP2_LIBRARY_NAME}
+			# PNG ARGS
+			-DPNG_LIBRARY_DIR:PATH=${PNG_LIBRARY_DIR}
+			-DPNG_LIBRARY_NAME:STRING=${PNG_LIBRARY_NAME}
+			# ZLIB ARGS
+			-DZLIB_LIBRARY_DIR:PATH=${ZLIB_LIBRARY_DIR}
+			-DZLIB_LIBRARY_NAME:STRING=${ZLIB_LIBRARY_NAME}
+			# Use the configure script
+			-P ${${proj}_PREBUILD_CONFIGURE_SCRIPT}
+
+		DEPENDEES patch
+		DEPENDERS configure
+	)
+
+	# Generate alias headers
+	SET( ${proj}_GENERATE_ALIAS_SCRIPT ${CMAKE_CURRENT_LIST_DIR}/External_FontConfig_generatealias.cmake )
+	ExternalProject_Add_Step( ${proj} generate_alias
+		COMMAND ${CMAKE_COMMAND}
+			# CMake ARGS
+			-DSOURCE_DIR:PATH=${${proj}_SOURCE_DIR}
+			# Use the configure script
+			-P ${${proj}_GENERATE_ALIAS_SCRIPT}
+
+		DEPENDEES run_autogen_sh
+		DEPENDERS configure
+	)
+ENDIF()
 
 ### --- Set binary information
 SET( FONTCONFIG_DIR ${${proj}_INSTALL_DIR} )
